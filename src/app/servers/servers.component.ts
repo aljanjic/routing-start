14a512223e46fc0ab83b4395b25670752ed5d3fb
aljanjic <janjic_aljosa@hotmail.com>
2023-06-05 22:44:46 +0200

    Fetching Route parameters

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -20,7 +20,8 @@ export class ServersComponent implements OnInit {
 
 
   onReload(){
-    this.router.navigate(['servers'], {relativeTo: this.route})
+    // this.router.navigate(['servers'], {relativeTo: this.route})
+    console.log('click')
   }
 
 }
