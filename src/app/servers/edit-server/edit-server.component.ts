41436c35aaf4a1357e900c4cb60a3f67a13467e8
aljanjic <janjic_aljosa@hotmail.com>
2023-06-07 09:36:55 +0200

    Adding pageNotFound and exporting routes

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -20,17 +20,16 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     // console.log(this.route.snapshot.queryParams)
     // console.log(this.route.snapshot.fragment)
-    
+    // this.route.fragment.subscribe(() => {})
+
     this.route.queryParams.subscribe((queryParams:Params)=>{
       this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
-      console.log(queryParams['allowEdit'])
     })
 
-    this.route.fragment.subscribe(() => {})
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id']    
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
-    console.log(this.allowEdit)
   }
 
   onUpdateServer() {
