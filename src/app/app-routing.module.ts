dc522a7c2e1a24caecd3c61668a08dec0ff5a927
aljanjic <janjic_aljosa@hotmail.com>
2023-06-08 09:04:58 +0200

    implementing canDeacivateGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
 import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 
 
@@ -23,7 +24,7 @@ const appRoutes: Routes = [
     component: ServersComponent, 
     children:[
       {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent}
+      {path: ':id/edit', component: EditServerComponent, canDeactivate:[CanDeactivateGuard]}
     ]},
     {path: 'not-found', component: PageNotFoundComponent},
     {path: '**', redirectTo: 'not-found'}
